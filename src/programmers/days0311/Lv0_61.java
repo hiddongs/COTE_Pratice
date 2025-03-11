7b219fca9c8edbafca3083740ddc312d97fce112
SIST52 <SIST52@sist>
2025-03-11 17:46:52 +0900

    sad

--- src/programmers/days0311/Lv0_61.java
+++ src/programmers/days0311/Lv0_61.java
@@ -1,8 +1,7 @@
-package programmers.solution;
-
-import java.util.Arrays;
+package programmers.days0311;
 
 /*
+
 문제 설명
 정수 리스트 num_list와 정수 n이 주어질 때, num_list를 n 번째 원소 이후의 원소들과 n 번째까지의 원소들로 나눠 n 번째 원소 이후의 원소들을 n 번째까지의 원소들 앞에 붙인 리스트를 return하도록 solution 함수를 완성해주세요.
 
@@ -14,17 +13,7 @@ import java.util.Arrays;
 num_list	         n	    result
 [2, 1, 6]	         1	    [1, 6, 2]
 [5, 2, 1, 7, 5]	     3	    [7, 5, 5, 2, 1]
-
  */
-class Solution {
-	public int[] solution(int[] num_list, int n) {
-        int[] answer = {};
-        
-        
-        return answer;
-    }
+public class Lv0_61 {
+
 }
-
-
-
-	
